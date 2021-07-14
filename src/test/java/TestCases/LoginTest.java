95671e7b1120c5b1be9d80151f51be9461bde408
unknown <shivvasrinath@gmail.com>
2021-07-14 19:40:57 +0530

    Initial commit

---
+++ src/test/java/TestCases/LoginTest.java
@@ -0,0 +1,36 @@
+package TestCases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+	
+	public static WebDriver driver;
+	
+	@BeforeSuite
+	public void setUp()
+	{
+		driver = new FirefoxDriver();
+	}
+	
+	@Test
+	
+	public void doLogin()
+	{
+		driver.get("http://www.gmail.com");
+		driver.findElement(By.id("Email")).sendKeys("abc@gmail.com");
+		driver.findElement(By.id("Passwd")).sendKeys("abc@gmail.com");
+				
+	}
+	
+	@AfterSuite
+	public void tearDown() 
+	{
+		driver.quit();
+	}
+
+}
